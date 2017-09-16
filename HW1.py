7603e0bd9090de4bff4451842eaf64df67485112
John Vu <31754493+jv2764@users.noreply.github.com>
2017-09-15 23:13:53 -0700

    Add files via upload

---
+++ HW1.py
@@ -0,0 +1,82 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+a=5
+b=6
+print a and b
+
+
+# In[ ]:
+
+greeting= Hello
+person= Oscar
+print
+
+
+
+
+# In[ ]:
+
+greeting=Hello
+person=Oscar
+print personalgreeting
+
+
+# In[ ]:
+
+input ask user to enter his/her age
+
+
+# In[ ]:
+
+input ask user to enter his/her age
+response=age
+
+
+# In[ ]:
+
+if age is greater or less than 35
+print age
+
+
+# In[ ]:
+
+if age is greater or less than 35
+print age
+if value of age is greater than 35
+then display age is greater than 35
+if value of age is not greater than 35
+then display age is not greater than 35
+
+
+# In[ ]:
+
+H=10
+H divide by 4
+number result divide by 4
+modulas of number by 4
+
+
+# In[ ]:
+
+number=101.35
+number divide by 4
+number result divide by 4
+modulas of number by 4
+
+
+# In[ ]:
+
+myage=21
+yourage=25
+print myage==yourage
+compareresult=result
+hisage=25
+myage==hisage
+print result
+
+
+
+
